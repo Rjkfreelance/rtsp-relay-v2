051983fbf828c4fe8c7ee93dcd704ddc343b395f
Rjkfreelance <phpteach@outlook.com>
2020-12-01 10:56:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get('/', (req, res) =>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>RTSP IP Camera Streaming Example</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
